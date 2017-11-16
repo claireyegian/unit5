cdbbe5598e631d888f6dd85cfeba276860916c82
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-16 14:48:56 -0500

    Updated from Brython Server: 11/16/2017 2:49:02 PM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -11,9 +11,9 @@ def mouseClick(event):
 def changeColor():
     num1 = randint(0,3)
     
-    colorList = ['0x00ff63,1', '0xff4563,1', '0x004300,1', '0xff00ff,1']
+    colorList = [Color(0x00ff63,1), Color(0xff4563,1), Color(0x004300,1), Color(0xff00ff,1)]
     
-    color = Color(colorlist[num1])
+    color = colorList[num1]
     colorBox = RectangleAsset(1000,600,LineStyle(1,color),color)
     Sprite(colorBox)
 
