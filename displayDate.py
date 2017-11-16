b769aca69818f23b34c05601386dff1a88a3a87a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-16 14:52:13 -0500

    Updated from Brython Server: 11/16/2017 2:52:19 PM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,12 @@
-d
+#Claire Yegian
+#11/15/17
+#displayDate.py - displays current date
+
+from datetime import date
+
+today = datetime.date.today()
+day = today.day
+month = today.month
+year = today.year
+
+print(day, month, year)
