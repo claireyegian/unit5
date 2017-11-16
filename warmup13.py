d66fa4323d9145b8d9052ae55f87b28f749adf8a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-16 14:20:51 -0500

    Updated from Brython Server: 11/16/2017 2:20:57 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,15 @@
+#Claire Yegian
+#11/16/17
+#warmup13.py - makes list and prints min, max, and sum
 
+from random import randint
+
+numbers = []
+i = 0
+while i<20:
+    numbers.append(randint(9,99))
+    i += 1
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
