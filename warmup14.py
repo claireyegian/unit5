8d60f251bc2de4e7180f3e7813a86cf92c118322
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-17 14:37:11 -0500

    Updated from Brython Server: 11/17/2017 2:37:23 PM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,6 @@
+#Claire Yegian
+#11/17/17
+#warmup14.py - takes name input and prints initials
 
+name = input('Enter your first and last name: ').split()
+print('Your initials are '+name[0]+'.'+name[0]
