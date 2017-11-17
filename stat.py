e7f34239e5743deb41cc5a4fb1b1f2629f149d23
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-17 14:54:29 -0500

    Updated from Brython Server: 11/17/2017 2:54:41 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,18 @@
+#Claire Yegian
+#11/17/17
+#stat.py - outputs min, max, median, mean, and mode of list of numbers
 
+numbers = []
+i = ''
+while i!='q':
+    i = input('Enter a list of numbers: ')
+    if i!='q':
+        numbers.append(float(i))
+
+numbers.sort()
+print('Min:',min(numbers))
+print('Max:',max(numbers))
+if len(numbers)%2==0:
+    print('Median:',numbers[len(numbers)/2 - 1],',',numbers[len(numbers)/2])
+print('Mean:',sum(numbers)/len(numbers))
+print('Mode:',!!!!!!!!!!!!!!!!!!!)
