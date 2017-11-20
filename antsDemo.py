55e98961812fe8f43ae5dd10b4f97f6531349742
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-20 14:28:21 -0500

    Updated from Brython Server: 11/20/2017 2:28:34 PM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,22 @@
+#Claire Yegian
+#11/20/17
+#antsDemo.py - how to use lists with graphics
 
+from ggame import *
+from random import randint
+
+WIDTH = 1000 #constant throughout program so in all caps
+HEIGHT = 600
+
+if __name__ == '__main__':
+    
+    red = Color(0xFF0000,1)
+    ant = CircleAsset(10,LineStyle(1,red),red)
+    
+    data = {}
+    data['antList'] = []
+    
+    for i in range(10):
+        data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
+    
+    App().run()
