020cad97bacd53d452632649b43537e177d6da01
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-20 14:54:01 -0500

    Updated from Brython Server: 11/20/2017 2:54:14 PM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,18 @@
+#Claire Yegian
+#11/20/17
+#toDoList.py - keeps track of things to do
 
+commandList = []
+
+while True:
+    command  = input('Enter your command and the item you are changing: ')
+    
+    if command[0:3] == 'add':
+        commandList.append(command[4:])
+    if command[0] == 'delete':
+        commandList.delete(command[7:])
+    if command[0] == 'print:
+        for item in commandList:
+            print(item)
+    if commandList[0] == 'quit':
+        quit
