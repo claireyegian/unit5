86dc122ec75fe6ccbfe9c5e6f7c2f5dd1946c0af
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-20 14:33:17 -0500

    Updated from Brython Server: 11/20/2017 2:33:30 PM

--- antsDemo.py
+++ antsDemo.py
@@ -8,6 +8,15 @@ from random import randint
 WIDTH = 1000 #constant throughout program so in all caps
 HEIGHT = 600
 
+def step():
+    for ant in data['antList']:
+        dx = randint(-4,3)
+        dy = randint(-4,3)
+        if ant.x + dx > 0 and ant.x + dx < WIDTH:
+            ant.x += dx
+        if ant.y + dy > 0 and ant.y + dy < HEIGHT:
+            ant.y += dy
+
 if __name__ == '__main__':
     
     red = Color(0xFF0000,1)
@@ -19,4 +28,4 @@ if __name__ == '__main__':
     for i in range(10):
         data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
     
-    App().run()
+    App().run(step)
