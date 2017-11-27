e26a7da1fb985c3ba1ff4d20885bcddda587f458
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-27 14:53:26 -0500

    Updated from Brython Server: 11/27/2017 2:53:31 PM

--- mergeSort.py
+++ mergeSort.py
@@ -1,1 +1,56 @@
+#Claire Yegian
+#11/27/17
+#mergeSort.py - implementation of merge sort
 
+from random import randint
+from time import time
+
+N = 1000 #how many numbers will be sorted
+
+"""function merge_sort(list m)
+    // Base case. A list of zero or one elements is sorted, by definition.
+    if length of m ≤ 1 then
+        return m
+
+    // Recursive case. First, divide the list into equal-sized sublists
+    // consisting of the first half and second half of the list.
+    // This assumes lists start at index 0.
+    var left := empty list
+    var right := empty list
+    for each x with index i in m do
+        if i < (length of m)/2 then
+            add x to left
+        else
+            add x to right
+
+    // Recursively sort both sublists.
+    left := merge_sort(left)
+    right := merge_sort(right)
+
+    // Then merge the now-sorted sublists.
+    return merge(left, right)"""
+
+def mySort(A):
+    
+    return A
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
