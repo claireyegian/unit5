4ae13b6042f1fc57bfa7030cfff443cb4197ffd0
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-27 00:13:01 -0500

    Update snow.py

--- snow.py
+++ snow.py
@@ -1,1 +1,16 @@
+#Claire Yegian
+#11/25/17
+#snow.py - creates image of falling snow
+
+from ggame import *
+
+black = Color(0x000000,1)
+white = Color(0xffffff,1)
+
+Rectangle = RectangleAsset(400,400,LineStyle(1,black),black)
+Rectangle2 = RectangleAsset(400,400,LineStyle(1,black),white)
+
+Sprite(Rectangle)
+Sprite(Rectangle2)
+App().run()
 
