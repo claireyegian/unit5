7a614f7ab149f92667724992f96b95e73d4475d5
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-30 14:09:59 -0500

    Updated from Brython Server: 11/30/2017 2:10:01 PM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,10 @@
+#Claire Yegian
+#11/30/17
+#warmup15.py - doubles list of numbers
 
+def double(numbers):
+    for item in numbers:
+        item = item*2
+    return(numbers)
+
+print(double([2,4,6]))
