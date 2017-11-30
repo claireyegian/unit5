93c0ad2f436687d83a9cac88a3e28c56bd2f3f20
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-11-30 14:49:17 -0500

    Updated from Brython Server: 11/30/2017 2:49:19 PM

--- snow.py
+++ snow.py
@@ -7,13 +7,9 @@ from random import randint
 
 def step():
     for item in flakeList:
-        for i in range(0,len(flakeList)-1):
-            flake = flakeList[i]
-            flake.y += 1
-            if flake.y > 15:
-                nextFlake= flakeList[i + 1]
-                nextFlake.y += 1
-            i += 1
+        item.y += 1
+        if item.y > 15:
+                step()
     
 if __name__ == '__main__':
     black = Color(0x000000,1)
